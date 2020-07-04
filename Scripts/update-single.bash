04c37e34ba51389104a7040fd76675de55d61ef9
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-07-04 18:33:36 +0200

    0.179.2 (singlefile)

---
+++ Scripts/update-single.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "Removing old dir"
+rm -rf $DIR/../Sources/POGOProtos
+cd $DIR/../../POGOProtos
+echo "Updating base repo"
+git pull
+echo "Generating .swift file"
+protoc --swift_out=$DIR/../Sources --swift_opt=Visibility=Public base/base.proto
+mv $DIR/../Sources/base $DIR/../Sources/POGOProtos
+mv $DIR/../Sources/POGOProtos/base.pb.swift $DIR/../Sources/POGOProtos/POGOProtos.pb.swift
+
+cd $DIR
+echo "Starting Swift Build"
+swift build
+if [ $? -eq 0 ]; then
+    echo Success!
+else
+    echo Failed!
+fi
