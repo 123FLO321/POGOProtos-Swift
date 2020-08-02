908e8744d13195590c92a41a70d73ce4ca0b025e
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-08-02 13:28:14 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "POGOProtos-Swift",
+    name: "POGOProtos",
     products: [
         .library(name: "POGOProtos", targets: ["POGOProtos"])
     ],
