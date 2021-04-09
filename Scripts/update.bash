ddc8cc2a929d23553e662e74b4b5deae68ad61c7
--=FurtiF™=-- <msntchat@hotmail.fr>
2021-04-09 21:39:53 +0200

    update and add help thanks SkOODaT

--- Scripts/update.bash
+++ Scripts/update.bash
@@ -13,7 +13,13 @@ mv out/single_file/swift/POGOProtos.Rpc.pb.swift $DIR/../Sources/POGOProtos/POGO
 #if need *.desc need one other folder as POGOProtos swift build bugs or need ignore compile it in package conf.
 #note all data in desc file refer to POGOProtos.Rpc but this need here cleaned for good package.
 #mv out/single_file/swift/POGOProtos.Rpc.desc $DIR/../Sources/POGOProtos/POGOProtos.desc
-#sed -i '' -e 's/POGOProtos_Rpc_//g' $DIR/../Sources/POGOProtos/POGOProtos.pb.swift
+#sed bug thanks SkOODaT
+## Need condition for check defaults 'macos'
+#if mac os
+sed -i '' -e 's/POGOProtos_Rpc_//g' $DIR/../Sources/POGOProtos/POGOProtos.pb.swift
+#if linux
+#sed -i 's/POGOProtos_Rpc_//g' $DIR/../Sources/POGOProtos/POGOProtos.pb.swift
+##
 
 cd $DIR
 echo "Starting Swift Build"
