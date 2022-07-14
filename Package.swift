6896e3c8c21f05278d95d857980fc3c2b63aa36d
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2022-07-14 18:31:48 +0200

    0.243.0 (#28)
    * 239.0 auto make.
    
    * 0.241.0
    
    * 243.0
    
    * uses protoc 3.21.2 build as ttps://github.com/apple/swift-protobuf tag 1.19.0
    
    all this seen ok.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "POGOProtos", targets: ["POGOProtos"])
     ],
     dependencies: [
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.18.0")
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.19.0")
     ],
     targets: [
         .target(name: "POGOProtos", dependencies: ["SwiftProtobuf"])
